93d964195e6735a52115cffb382f95f2e019824b
Rgordon94 <149901727+Rgordon94@users.noreply.github.com>
2023-11-06 14:04:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-# app.py
+from flask import Flask
+app = Flask(__name__)
 
-print("Hello World!")
+@app.route('/')
+def hello_world():
+   return 'Hello World!'
+
+if __name__ == '__main__':
+   app.run()
